2f1c6ba4a74912f2d0c1a2ec056031c0a634048f
JoaoVitorOliveiraa <jvoliveira30@gmail.com>
2024-12-10 01:04:20 -0300

    Atualização de dataset.

--- 2 - Classification/1_sgd_classifier.py
+++ 2 - Classification/1_sgd_classifier.py
@@ -3,16 +3,16 @@
 #==============================================================================
 
 #------------------------------------------------------------------------------
-# Importar a biblioteca Pandas, PyPlot e Path
+# Importar a biblioteca Pandas, Pyplot e Sklearn
 #------------------------------------------------------------------------------
 
-import numpy as np
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
 from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, accuracy_score, f1_score
+from sklearn.metrics import confusion_matrix, precision_score, recall_score
+from sklearn.metrics import f1_score, precision_recall_curve, roc_curve, roc_auc_score
+from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict
 
 #------------------------------------------------------------------------------
 # Importar o conjunto de dados Digits
@@ -56,52 +56,26 @@ print(dados_digits.describe())
 # Separar os atributos e o alvo, exibindo suas dimensões
 #------------------------------------------------------------------------------
 
-atributos = dados_digits.iloc[:, :-1].values
-rotulos = dados_digits.iloc[:, -1].values
+atributos = dados_digits.iloc[:, :-1].to_numpy()   # ou :-1].values
+rotulos = dados_digits.iloc[:, -1].to_numpy()      # ou :-1].values
 print("\n\n\t-----Dimensões-----")
 print(f"\nDimensão das features: {atributos.shape}")
 print(f"Dimensão dos rótulos: {rotulos.shape}\n")
 
 # ------------------------------------------------------------------------------
-#  Visualizar alguns digitos
+#  Visualizar o primeiro digito
 # ------------------------------------------------------------------------------
 
 # Cada imagem tem 8 x 8 pixels e cada característica representa a intensidade do
 # pixel, do 0 (branco) a 16 (preto).
 
-# Um imagem por digito (totalizando 10 imagens).
-# for index_digito in range(10):
-#     digito = atributos[index_digito]
-#     imagem_digito = digito.reshape(8, 8)
-#     plt.imshow(imagem_digito, cmap='binary')
-#     plt.show()
-
-# Uma figura armazenando as imagens dos 10 dígitos.
-plt.figure(figsize=(70, 50))
-for amostra in range(0, 10):
-    imagem_digito = plt.subplot(1, 10, amostra+1)
-    imagem_digito.set_title("Rótulo = %.0f" % rotulos[amostra])
-
-    imagem_digito.imshow(atributos[amostra, :].reshape(8, 8),
-                  # interpolation='spline16',
-                  # interpolation='nearest',
-                  interpolation='none',
-                  cmap='binary',
-                  vmin=0, vmax=16)
-    # plt.text(-8, 3, "y = %.2f" % y[i])
-
-    imagem_digito.set_xticks(())
-    imagem_digito.set_yticks(())
-
-plt.show()
-
-# ------------------------------------------------------------------------------
-#  Histograma dos dados
-#  Eixo vertical: Número de instâncias
-#  Eixo horizontal: Determinado intervalo valores
-# ------------------------------------------------------------------------------
-
-dados_digits.hist(bins=50, figsize=(35, 35))
+primeiro_digito = atributos[0]
+plt.figure(figsize=(8, 8))
+imagem_primeiro_digito = plt.subplot(111)
+imagem_primeiro_digito.set_title("Rótulo = %.0f" % rotulos[0])
+imagem_primeiro_digito.imshow(primeiro_digito.reshape(8, 8), cmap="binary")
+imagem_primeiro_digito.set_xticks(())
+imagem_primeiro_digito.set_yticks(())
 plt.show()
 
 # ------------------------------------------------------------------------------
@@ -113,149 +87,124 @@ dados_embaralhados = dados_digits.sample(frac=1, random_state=11012005)
 
 # ------------------------------------------------------------------------------
 # Criar os arrays de atributos e rotulos para um conjunto de treino e de teste
-# iniciais (que seram utilizados para treinar o modelo) e um outro conjunto de
-# treino e teste para o teste final do modelo
 # ------------------------------------------------------------------------------
 
-atributos_teste_inicial = dados_embaralhados.iloc[:1297, :-1].to_numpy()   # ou :-1].values
-rotulos_teste_inicial = dados_embaralhados.iloc[:1297, -1].to_numpy()    # ou -1].values
 
-atributos_teste_final = dados_embaralhados.iloc[1297:, :-1].to_numpy()    # ou :-1].values
-rotulos_teste_final = dados_embaralhados.iloc[1297:, -1].to_numpy()       # ou -1].values
-
-atributos_treino, atributos_teste, rotulos_treino, rotulos_teste = train_test_split(atributos_teste_inicial,
-                                                                                    rotulos_teste_inicial,
-                                                                                    test_size=0.35,
+atributos_treino, atributos_teste, rotulos_treino, rotulos_teste = train_test_split(atributos,
+                                                                                    rotulos,
+                                                                                    test_size=0.25,
                                                                                     random_state=11012005)
+
+# ------------------------------------------------------------------------------
+#  Transformando os rótulos de treino e teste para o classificador binário
+#  Somente será capaz de fazer distinções entre apenas duas classes, 0 e não 0
+# ------------------------------------------------------------------------------
+
+rotulos_treino_0 = (rotulos_treino==0)  # True para todos os 0s, False para todos os outros
+rotulos_teste_0 = (rotulos_teste==0)
 
 # -------------------------------------------------------------------------------
 # Treinar um classificador SGD com o conjunto de treino
 # -------------------------------------------------------------------------------
 
 classificador_sgd = SGDClassifier(random_state=11012005)
-classificador_sgd.fit(atributos_treino, rotulos_treino)
+#classificador_sgd.fit(atributos_treino, rotulos_treino_0); Treinamos abaixo com a validação cruzada
 
 # -------------------------------------------------------------------------------
-# Obter as respostas do classificador no mesmo conjunto onde foi treinado
+# Obtendo a acurácia dos 3 folds da validação cruzada
 # -------------------------------------------------------------------------------
 
-rotulos_resposta_treino = classificador_sgd.predict(atributos_treino)
+acuracias_cross = cross_val_score(classificador_sgd, atributos_treino, rotulos_treino_0, cv=3, scoring='accuracy')
+
+print("\n\n\t-----Acurácias da Validação Cruzada-----\n")
+for index in range(len(acuracias_cross)):
+    print(f"Acurácia {index+1}: {(100 * acuracias_cross[index]):.3f}%")
+
+print(f"Médias das Acurácias: {(100 * acuracias_cross.mean()):.3f}%")
 
 # -------------------------------------------------------------------------------
-# Obter as respostas do classificador no conjunto de teste
+# Predição em cada um dos folds
 # -------------------------------------------------------------------------------
 
-rotulos_resposta_teste = classificador_sgd.predict(atributos_teste)
-
-# -------------------------------------------------------------------------------
-# Verificar a acurácia do classificador
-# -------------------------------------------------------------------------------
-
-print("\n\t-----Classificador SGD (Dentro da amostra)-----\n")
-
-total = len(rotulos_treino)
-acertos = sum(rotulos_resposta_treino == rotulos_treino)
-erros = sum(rotulos_resposta_treino != rotulos_treino)
-
-print("\tTotal de amostras: ", total)
-print("\tRespostas corretas:", acertos)
-print("\tRespostas erradas: ", erros)
-
-acuracia = acertos / total
-
-print("\tAcurácia = %.1f %%" % (100*acuracia))
-print("\tTaxa Erro = %4.1f %%" % (100*(1-acuracia)))
-
-print("\n\t-----Classificador SGD (Fora da amostra)-----\n")
-
-total = len(rotulos_teste)
-acertos = sum(rotulos_resposta_teste == rotulos_teste)
-erros = sum(rotulos_resposta_teste != rotulos_teste)
-
-print("\tTotal de amostras: ", total)
-print("\tRespostas corretas:", acertos)
-print("\tRespostas erradas: ", erros)
-
-acuracia = acertos / total
-
-print("\tAcurácia  = %4.1f %%" % (100*acuracia))
-print("\tTaxa Erro = %4.1f %%" % (100*(1-acuracia)))
+rotulos_resposta_treino = cross_val_predict(classificador_sgd, atributos_treino, rotulos_treino_0, cv=3)
 
 # -------------------------------------------------------------------------------
 # Matriz de confusão
 # -------------------------------------------------------------------------------
 
-matriz_de_confusao = confusion_matrix(rotulos_resposta_teste, rotulos_teste)
+matriz_de_confusao = confusion_matrix(rotulos_treino_0, rotulos_resposta_treino)
 print('\n\n\t-----Matriz de Confusao-----\n\n', matriz_de_confusao)
 
 # -------------------------------------------------------------------------------
-# Calculo da precisão através da função precision_score(), usando a medida "macro"
+# Obtendo a precisão através da função precision_score()
 # -------------------------------------------------------------------------------
 
-precisao_score = precision_score(rotulos_teste, rotulos_resposta_teste, average='macro')
-print("\n\n\t-----Precisão - precision_score()-----\n\n\tPrecisão: ", precisao_score)
+precisao = precision_score(rotulos_treino_0, rotulos_resposta_treino)
+print(f'\n\n\t-----Precisão da Predição-----\n\nPrecisão: {(100*precisao):.3f}')
 
 # -------------------------------------------------------------------------------
-# Calculo da acurácia através da função accuracy_score()
+# Obtendo a revocação através da função recall_score()
 # -------------------------------------------------------------------------------
 
-acuracia_score = accuracy_score(rotulos_teste, rotulos_resposta_teste)
-print("\n\n\t-----Acurácia - accuracy_score()-----\n\n\tAcurácia: ", acuracia_score)
+revocacao = recall_score(rotulos_treino_0, rotulos_resposta_treino)
+print(f'\n\n\t-----Revocação da Predição-----\n\nRevocação: {(100*revocacao):.3f}')
 
 # -------------------------------------------------------------------------------
-# Calculo da revocação/sensibilidade (recall/sensitivity) através da função
-# recall_score(), usando a medida "macro"
+# Calculo da métrica F1 através da função recall_score()
+# "average" default, pois o classificador é binário
 # -------------------------------------------------------------------------------
 
-revocacao_score = recall_score(rotulos_teste, rotulos_resposta_teste, average='macro')
-print("\n\n\t-----Revocação/Sensibiliade - recall_score()-----\n\n\tRevocação: ", revocacao_score)
+metrica_f1 = f1_score(rotulos_treino_0, rotulos_resposta_treino)
+print(f"\n\n\t-----F1 da Predição-----\n\nF1: {(100*metrica_f1):.3f}%")
 
 # -------------------------------------------------------------------------------
-# Calculo da métrica F1 através da função recall_score(), usando a medida "macro"
+# Obtendo os scores de decisão de todas as instâncias no conjunto de treinamento
+# "decision_function()" retorna um score para cada instância e, em seguida, faz as
+# predições com base nesses scores usando qualquer limiar desejado
 # -------------------------------------------------------------------------------
 
-metrica_f1 = f1_score(rotulos_teste, rotulos_resposta_teste, average='macro')
-print("\n\n\t-----F1 - f1_score()-----\n\n\tF1: ", metrica_f1)
+scores_decisao = cross_val_predict(classificador_sgd, atributos_treino, rotulos_treino_0, cv=3, method='decision_function')
+print(f"\n\n\t-----Scores de decisão de todas as instâncias no conjunto de treinamento-----\n\nScores: {scores_decisao}")
 
 # -------------------------------------------------------------------------------
-# Verificar os erros cometidos pelo classificador
+# Calculando a precisão e a revocação em todos os limiares possíveis através da
+# função precision_recall_curve
 # -------------------------------------------------------------------------------
 
-# Armazena a lista com as amostras dos erros
-indice_erro = np.where(rotulos_resposta_teste != rotulos_teste)[0]
-quantidade_erros = len(indice_erro)
+precisoes, revocacoes, limiares = precision_recall_curve(rotulos_treino_0, scores_decisao)
 
-# Exibe uma figura com os digitos dos erros.
-plt.figure(figsize=(30, 30))
-for i in range(quantidade_erros):
+# -------------------------------------------------------------------------------
+# Plotando a precisão e a revocação como funções do valor limiar
+# -------------------------------------------------------------------------------
 
-    if quantidade_erros % 2 == 0:
-        quantidade_linhas_figura = 2
-        quantidade_colunas_figura = int(quantidade_erros / 2)
-
-    elif quantidade_erros % 3 == 0:
-        quantidade_linhas_figura = 3
-        quantidade_colunas_figura = int(quantidade_erros / 3)
-
-    elif quantidade_erros % 5 == 0:
-        quantidade_linhas_figura = 5
-        quantidade_colunas_figura = int(quantidade_erros / 5)
-
-    else:
-        quantidade_linhas_figura = 3
-        quantidade_colunas_figura = 10
-
-    plot_erro = plt.subplot(quantidade_linhas_figura, quantidade_colunas_figura, i + 1)
-    plot_erro.set_title("gabarito = %d ; resposta = %d" %
-                        (rotulos_teste[indice_erro[i]], rotulos_resposta_teste[indice_erro[i]]))
-
-    plot_erro.imshow(atributos_teste[indice_erro[i], :].reshape(8, 8),
-                     # interpolation='spline16',
-                     interpolation='nearest',
-                     cmap='binary',
-                     vmin=0, vmax=16)
-
-    plot_erro.set_xticks(())
-    plot_erro.set_yticks(())
-
+plt.plot(limiares, precisoes[:-1], "b--", label="Precisão")
+plt.plot(limiares, revocacoes[:-1], "g-", label="Revocações")
+plt.xlabel("Limiares")
+plt.legend()
 plt.show()
+
+# -------------------------------------------------------------------------------
+# Calculando o TPR (True Positives) e o FPR (False Positives) para diversos
+# valores de limiares, a fim de plotar a curva ROC
+# -------------------------------------------------------------------------------
+
+falsos_positivos, verdadeiros_positivos, limiares = roc_curve(rotulos_treino_0, scores_decisao)
+
+# -------------------------------------------------------------------------------
+# Plotando a curva ROC
+# -------------------------------------------------------------------------------
+
+plt.plot(falsos_positivos, verdadeiros_positivos, linewidth=2)
+plt.plot([0,1], [0,1], 'k--')       # Diagonal tracejada
+plt.xlabel("Taxa de Falsos Positivos")
+plt.ylabel("Taxa de Verdadeiros Positivos")
+plt.grid()
+plt.legend()
+plt.show()
+
+# -------------------------------------------------------------------------------
+# Calculando a área sob a curva (AUC)
+# -------------------------------------------------------------------------------
+
+area_sob_curva = roc_auc_score(rotulos_treino_0, scores_decisao)
+print(f"\n\n\t-----Área sob a curva (AUC)-----\n\nÁrea: {area_sob_curva}")
